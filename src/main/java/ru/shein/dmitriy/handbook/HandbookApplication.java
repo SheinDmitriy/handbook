baa7169ad1955a058c42b896c7fa7198a4a4fe49
SheinDmitriy <dmitriy.shein86@gmail.com>
2021-04-05 18:27:24 +0500

    Initial commit

---
+++ src/main/java/ru/shein/dmitriy/handbook/HandbookApplication.java
@@ -0,0 +1,13 @@
+package ru.shein.dmitriy.handbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandbookApplication.class, args);
+    }
+
+}
